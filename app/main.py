4eb76aefd1da2ac221c254c385606a571f7fea84
wole <wole1702@gmail.com>
2026-02-05 12:14:33 +0100

    none

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://de-doc.vercel.app/", "http://127.0.0.1:8000", "http://localhost:8000", "http://127.0.0.1:3000", "http://localhost:3000" ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
